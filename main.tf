692b01da5ebb3f042477f02c96df3be6be53d711
Abdelhak FRIHA <abdelhak.friha@metanext.com>
2020-07-16 23:32:53 +0200

    ansible prep

--- main.tf
+++ main.tf
@@ -5,22 +5,7 @@ provider "aws" {
   version = "~> 2.0"
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-resource "aws_vpc" "webinar" {
+resource "aws_vpc" "webinar_vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
@@ -28,44 +13,60 @@ resource "aws_vpc" "webinar" {
     Name = "Webinar_vpc"
   }
 }
-resource "aws_subnet" "webinar" {
-  vpc_id     = aws_vpc.webinar.id
+resource "aws_subnet" "webinar_subnet" {
+  vpc_id     = aws_vpc.webinar_vpc.id
   cidr_block = "10.0.0.0/24"
 
   tags = {
     Name = "Webinar_subnet"
   }
 }
-resource "aws_security_group" "base" {
-  name        = "Base SG"
+# Internet gateway
+resource "aws_internet_gateway" "webinar_gw" {
+  vpc_id = aws_vpc.webinar_vpc.id
+}
 
-  # Outbound HTTPS
-  egress {
-    from_port   = 443
-    to_port     = 443
+# Public Route Table
+resource "aws_route_table" "webinar_public" {
+  vpc_id = aws_vpc.webinar_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.webinar_gw.id
+  }
+}
+
+# Route Table Assoc for Webinar_subnet
+resource "aws_route_table_association" "webinar_rt" {
+  subnet_id      = aws_subnet.webinar_subnet.id
+  route_table_id = aws_route_table.webinar_public.id
+}
+
+resource "aws_security_group" "webinar_sg" {
+  name        = "Webinar SG"
+  vpc_id      = aws_vpc.webinar_vpc.id
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  # Outbound HTTP
-  egress {
+  ingress {
+    description = "HTTP"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Allow inbound SSH
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-    self        = false
   }
-
-  vpc_id = aws_vpc.webinar.id
-
 }
 
 resource "aws_key_pair" "hako" {
@@ -74,15 +75,18 @@ resource "aws_key_pair" "hako" {
 }
 
 resource "aws_instance" "webinar" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-  subnet_id              = aws_subnet.webinar.id
-  key_name               = aws_key_pair.hako.id
-  vpc_security_group_ids = [aws_security_group.base.id]
+  ami                    = "ami-0e11cbb34015ff725"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.webinar_subnet.id
+  key_name               = aws_key_pair.hako.key_name
+  vpc_security_group_ids = [aws_security_group.webinar_sg.id]
   associate_public_ip_address= true
 
 
   tags = {
     Name = "Webinar"
   }
+}
+output "ip" {
+  value = aws_instance.webinar.public_ip
 }
